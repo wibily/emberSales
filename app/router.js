a7560bfd25b33622557f29dadc85da55c93df3bf
wibily <wibily@gmail.com>
2015-06-21 14:27:03 +1000

    share model bound

--- app/router.js
+++ app/router.js
@@ -6,14 +6,14 @@ var Router = Ember.Router.extend({
 });
 
 
-
-Router.map(function() {
-  this.resource('share', { path: '/' }, function () {
+Router.map(function () {
+  this.resource('share',{ path: '/share/:uuid' }, function () {
     this.route('comments');
     this.route('participants');
     this.route('options');
   });
 });
 
+//102f-3b2b-b087-4a19-8632-a62a-6fa3-2b99
 
 export default Router;
