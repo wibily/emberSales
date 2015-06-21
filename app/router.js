119325980f16be3c7d950cca34a4b308fe2b08d2
wibily <wibily@gmail.com>
2015-06-21 12:29:49 +1000

    initial commit

--- app/router.js
+++ app/router.js
@@ -5,7 +5,15 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+
+
 Router.map(function() {
+  this.resource('share', { path: '/' }, function () {
+    this.route('comments');
+    this.route('participants');
+    this.route('options');
+  });
 });
+
 
 export default Router;
